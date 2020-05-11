fcd043a72e0e6ec72fb53d031dff35f43456d90e
jordi <jsabat@gmail.com>
2020-05-11 15:41:24 +0100

    Add Sign in form event and state classes

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_firebase_ddd_notes/domain/auth/i_auth_facade.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/auth/auth_failure.dart';
+import '../../../domain/auth/value_objects.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade);
+
+  @override
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
