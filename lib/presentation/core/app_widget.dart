0641282c60dafb72ec30c825d30196323b0ce9c3
jordi <jsabat@gmail.com>
2020-06-01 14:22:32 +0100

    Navigation based on Auth State

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,22 +1,34 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_ddd_notes/application/auth/auth_bloc.dart';
 
-import '../sign_in/sign_in_page.dart';
+import '../../injection.dart';
+import '../routes/router.gr.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-          primaryColor: Colors.green[800],
-          accentColor: Colors.blueAccent,
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-          )),
-      home: const SignInPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckResquested()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+            primaryColor: Colors.green[800],
+            accentColor: Colors.blueAccent,
+            inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            )),
+        builder: ExtendedNavigator(router: Router()),
+      ),
     );
   }
 }
